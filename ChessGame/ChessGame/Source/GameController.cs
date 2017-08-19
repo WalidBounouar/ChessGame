4f81cdae142c4e1f67b5fd51b9d12cd175f917bb
WalidBounouar <walid.bounouar97@gmail.com>
2017-08-19 18:02:05 -0400

    started work on GameController and TextView

--- ChessGame/ChessGame/Source/GameController.cs
+++ ChessGame/ChessGame/Source/GameController.cs
@@ -8,6 +8,32 @@ namespace ChessGame.Source {
 
     class GameController {
 
+        /// <summary>
+        /// Game model - Board class
+        /// </summary>
+        private Board board;
+
+        /// <summary>
+        /// Texy View
+        /// </summary>
+        private TextView textView;
+
+        /// <summary>
+        /// The player at the moment
+        /// 0 --> white
+        /// 1 --> black
+        /// </summary>
+        private int turn;
+
+        public GameController() {
+
+            this.board = new Board();
+            this.textView = new TextView(this.board, this);
+
+            this.turn = 0; //white starts
+
+        }
+
 
 
     }
--- ChessGame/ChessGame/Source/TextView.cs
+++ ChessGame/ChessGame/Source/TextView.cs
@@ -8,7 +8,16 @@ namespace ChessGame.Source {
 
     class TextView {
 
+        Board board;
 
+        GameController controller;
+
+        public TextView(Board board, GameController controller) {
+
+            this.board = board;
+            this.controller = controller;
+
+        }
 
     }
 

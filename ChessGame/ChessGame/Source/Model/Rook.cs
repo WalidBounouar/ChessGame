905dab88bdc24d0565ba52b15c481e7dff7b6774
WalidBounouar <walid.bounouar97@gmail.com>
2017-08-22 21:18:22 -0400

    Coded getDestinations of Rook.

--- ChessGame/ChessGame/Source/Model/Rook.cs
+++ ChessGame/ChessGame/Source/Model/Rook.cs
@@ -21,7 +21,104 @@ namespace ChessGame.Source.Model {
         }
 
         public override void getDestinations(int posX, int posY) {
-            throw new NotImplementedException();
+            //throw new NotImplementedException();
+
+            int tmpX;
+            int tmpY;
+            BoardSpace tmpSpace;
+
+            //up seach
+            tmpX = posX;
+            tmpY = posY + 1;
+
+            while (tmpY < Board.GAMESIZE
+                && !this.Board.getBoardSpace(tmpX, tmpY).Occupied) {
+
+                tmpSpace = this.Board.getBoardSpace(tmpX, tmpY);
+                tmpSpace.IsPossibleDestination = true;
+
+                tmpY++;
+
+            }
+            //Check in case the loop stoped because we found occupied space
+            if (tmpY < Board.GAMESIZE
+                && this.Board.getBoardSpace(tmpX, tmpY).Occupied
+                && this.Board.getBoardSpace(tmpX, tmpY).Piece.Color != this.Color) {
+
+                tmpSpace = this.Board.getBoardSpace(tmpX, tmpY);
+                tmpSpace.IsPossibleDestination = true;
+
+            }
+
+            //down seach
+            tmpX = posX;
+            tmpY = posY - 1;
+
+            while (tmpY >= 0
+                && !this.Board.getBoardSpace(tmpX, tmpY).Occupied) {
+
+                tmpSpace = this.Board.getBoardSpace(tmpX, tmpY);
+                tmpSpace.IsPossibleDestination = true;
+
+                tmpY--;
+
+            }
+            //Check in case the loop stoped because we found occupied space
+            if (tmpY >= 0
+                && this.Board.getBoardSpace(tmpX, tmpY).Occupied
+                && this.Board.getBoardSpace(tmpX, tmpY).Piece.Color != this.Color) {
+
+                tmpSpace = this.Board.getBoardSpace(tmpX, tmpY);
+                tmpSpace.IsPossibleDestination = true;
+
+            }
+
+            //right seach
+            tmpX = posX + 1;
+            tmpY = posY;
+
+            while (tmpX < Board.GAMESIZE
+                && !this.Board.getBoardSpace(tmpX, tmpY).Occupied) {
+
+                tmpSpace = this.Board.getBoardSpace(tmpX, tmpY);
+                tmpSpace.IsPossibleDestination = true;
+
+                tmpX++;
+
+            }
+            //Check in case the loop stoped because we found occupied space
+            if (tmpX < Board.GAMESIZE
+                && this.Board.getBoardSpace(tmpX, tmpY).Occupied
+                && this.Board.getBoardSpace(tmpX, tmpY).Piece.Color != this.Color) {
+
+                tmpSpace = this.Board.getBoardSpace(tmpX, tmpY);
+                tmpSpace.IsPossibleDestination = true;
+
+            }
+
+            //left seach
+            tmpX = posX - 1;
+            tmpY = posY;
+
+            while (tmpX >= 0
+                && !this.Board.getBoardSpace(tmpX, tmpY).Occupied) {
+
+                tmpSpace = this.Board.getBoardSpace(tmpX, tmpY);
+                tmpSpace.IsPossibleDestination = true;
+
+                tmpX--;
+
+            }
+            //Check in case the loop stoped because we found occupied space
+            if (tmpX >= 0
+                && this.Board.getBoardSpace(tmpX, tmpY).Occupied
+                && this.Board.getBoardSpace(tmpX, tmpY).Piece.Color != this.Color) {
+
+                tmpSpace = this.Board.getBoardSpace(tmpX, tmpY);
+                tmpSpace.IsPossibleDestination = true;
+
+            }
+
         }
     }
 
